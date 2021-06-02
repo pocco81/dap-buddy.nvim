66d217b2e36f318588c8f86fbfd9d747c995378e
kdav5758 <kdav5758@gmail.com>
2021-06-01 21:48:31 -0500

    renamed function

--- lua/dap-install/tools/tool_uninstall/init.lua
+++ lua/dap-install/tools/tool_uninstall/init.lua
@@ -8,7 +8,7 @@ local utils_tbl = require("dap-install.utils.tables.init")
 local utils_paths = require("dap-install.utils.paths.init")
 local dbg_list = require("dap-install.debuggers_list").debuggers
 
-function M.install_debugger(debugger)
+function M.uninstall_debugger(debugger)
     if not utils_tbl.tbl_has_element(dbg_list, debugger) then
         if (utils_paths.assert_dir(dbg_path) == 1) then
             if fn.confirm("Do you want to uinstall the debugger " .. debugger .. "?", "&Yes\n&Cancel") ~= 1 then
