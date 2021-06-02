70889de5093ede0d00587e0ecf82369348971b64
kdav5758 <kdav5758@gmail.com>
2021-06-02 09:09:38 -0500

    specified index and added prints

--- lua/dap-install/init.lua
+++ lua/dap-install/init.lua
@@ -15,20 +15,24 @@ function M.config(debugger, config)
 
     if not next(config) == nil then
         for opt, _ in pairs(config) do
+			print("Opt = "..opt)
             if (opt == "adapters") then
                 for inner_opt, _ in pairs(opt["adapters"]) do
+					print("Inner opt (adapters) = "..inner_opt)
                     if (dbg.config["config"][opt]["adapters"][inner_opt] ~= nil) then -- not nil
                         config.options[opt]["adapters"][inner_opt] = config[opt][inner_opt]
                     end
                 end
             elseif (opt == "configurations") then
-                for inner_opt, _ in pairs(opt["configurations"]) do
-                    if (dbg.config["config"][opt]["configurations"][inner_opt] ~= nil) then -- not nil
-                        config.options[opt]["configurations"][inner_opt] = config[opt][inner_opt]
+				-- should iterate through index one
+                for inner_opt, _ in pairs(opt["configurations"][1]) do
+					print("Inner opt (configurations) = "..inner_opt)
+                    if (dbg.config["config"][opt]["configurations"][1][inner_opt] ~= nil) then -- not nil
+                        config.options[opt]["configurations"][1][inner_opt] = config[opt][1][inner_opt]
                     end
                 end
 			else
-				print("Opt "..tostring(opt).." was not recognized")
+				print("Opt "..tostring(opt).." was not recognized!")
             end
         end
     end
