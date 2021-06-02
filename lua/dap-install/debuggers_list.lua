9e8e497a619ea112ffef5500ac62b1af40b152bf
kdav5758 <kdav5758@gmail.com>
2021-06-02 08:59:38 -0500

    changed debugpy name

--- lua/dap-install/debuggers_list.lua
+++ lua/dap-install/debuggers_list.lua
@@ -5,7 +5,7 @@ local opts = require("dap-install.config").options
 local M = {}
 
 M.debuggers = {
-    ["debugpy"] = {"dap-install.debuggers._dc_debugpy", opts.installation_path.."debugpy/"}
+    ["debugpy_dbg"] = {"dap-install.debuggers._dc_debugpy", opts.installation_path.."debugpy_dbg/"}
 }
 
 return M
