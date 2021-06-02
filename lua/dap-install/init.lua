7425f574c9828397d7af1ca801c6de0ec2140652
kdav5758 <kdav5758@gmail.com>
2021-06-02 10:40:16 -0500

    added option to check if the debugger is supported

--- lua/dap-install/init.lua
+++ lua/dap-install/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local dap = require("dap")
+local utils_tbl = require("dap-install.utils.tables.init")
 
 function M.setup(custom_opts)
     require("dap-install.config").set_options(custom_opts)
@@ -18,26 +19,30 @@ function M.config(debugger, config)
     config = config or {}
     local dbg_list = require("dap-install.debuggers_list").debuggers
 
-    local dbg = require(dbg_list[debugger][1])
+    if utils_tbl.tbl_has_element(dbg_list, debugger) then
+        local dbg = require(dbg_list[debugger][1])
 
-    if next(config) then
-        for opt, _ in pairs(config) do
-            if (opt == "adapters") then
-                for inner_opt, _ in pairs(config[opt]) do
-                    if (dbg.config[opt][inner_opt] ~= nil) then -- not nil
-                        dbg.config[opt][inner_opt] = config[opt][inner_opt]
+        if next(config) then
+            for opt, _ in pairs(config) do
+                if (opt == "adapters") then
+                    for inner_opt, _ in pairs(config[opt]) do
+                        if (dbg.config[opt][inner_opt] ~= nil) then -- not nil
+                            dbg.config[opt][inner_opt] = config[opt][inner_opt]
+                        end
                     end
-                end
-            elseif (opt == "configurations") then
-                for inner_opt, _ in pairs(config[opt][1]) do
-                    if (dbg.config[opt][1][inner_opt] ~= nil) then -- not nil
-                        dbg.config[opt][1][inner_opt] = config[opt][1][inner_opt]
+                elseif (opt == "configurations") then
+                    for inner_opt, _ in pairs(config[opt][1]) do
+                        if (dbg.config[opt][1][inner_opt] ~= nil) then -- not nil
+                            dbg.config[opt][1][inner_opt] = config[opt][1][inner_opt]
+                        end
                     end
+                else
+                    print("Config option" .. tostring(opt) .. " was not recognized!")
                 end
-            else
-                print("Config option" .. tostring(opt) .. " was not recognized!")
             end
         end
+	else
+		print("DAPInstall: The debugger "..debugger.." is unsupported")
     end
 
     call_on_dap(debugger, dbg_list)
