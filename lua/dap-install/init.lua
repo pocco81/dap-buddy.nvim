e3e065728cc971a5447f2b83e879984feb568815
kdav5758 <kdav5758@gmail.com>
2021-06-02 09:59:50 -0500

    removed more prints

--- lua/dap-install/init.lua
+++ lua/dap-install/init.lua
@@ -20,24 +20,16 @@ function M.config(debugger, config)
 
     local dbg = require(dbg_list[debugger][1])
 
-    print("Debugger = " .. debugger .. "; Config = " .. tostring(config))
-
     if next(config) then
-        print("Got here!!")
         for opt, _ in pairs(config) do
-            print("Opt = " .. opt)
             if (opt == "adapters") then
-                print("adapters = " .. tostring(config[opt]))
                 for inner_opt, _ in pairs(config[opt]) do
-                    print("Inner opt (adapters) = " .. inner_opt)
                     if (dbg.config[opt][inner_opt] ~= nil) then -- not nil
                         dbg.config[opt][inner_opt] = config[opt][inner_opt]
                     end
                 end
             elseif (opt == "configurations") then
-                print("Config = " .. tostring(config[opt]))
                 for inner_opt, _ in pairs(config[opt][1]) do
-                    print("Inner opt (configurations) = " .. inner_opt)
                     if (dbg.config[opt][1][inner_opt] ~= nil) then -- not nil
                         dbg.config[opt][1][inner_opt] = config[opt][1][inner_opt]
                     end
