2224e835505206b2be4d2cdd032a676b6d91ab38
kdav5758 <kdav5758@gmail.com>
2021-06-02 09:23:27 -0500

    testing adapters mappings

--- lua/dap-install/init.lua
+++ lua/dap-install/init.lua
@@ -19,15 +19,15 @@ function M.config(debugger, config)
         for opt, _ in pairs(config) do
             print("Opt = " .. opt)
             if (opt == "adapters") then
-                --                 for inner_opt, _ in pairs(opt["adapters"]) do
-                -- print("Inner opt (adapters) = "..inner_opt)
-                --                     if (dbg.config["config"][opt]["adapters"][inner_opt] ~= nil) then -- not nil
-                --                         config.options[opt]["adapters"][inner_opt] = config[opt][inner_opt]
-                --                     end
-                --                 end
+                print("adapters = " .. tostring(config[opt]))
+                for inner_opt, _ in pairs(config[opt][1]) do
+                    print("Inner opt (adapters) = " .. inner_opt)
+                    if (dbg.config[opt][1][inner_opt] ~= nil) then -- not nil
+                        dbg.config[opt][1][inner_opt] = config[opt][1][inner_opt]
+                    end
+                end
             elseif (opt == "configurations") then
-                -- should iterate through index one
-                print("Config -1 = " .. tostring(config[opt]))
+                print("Config = " .. tostring(config[opt]))
                 for inner_opt, _ in pairs(config[opt][1]) do
                     print("Inner opt (configurations) = " .. inner_opt)
                     if (dbg.config[opt][1][inner_opt] ~= nil) then -- not nil
