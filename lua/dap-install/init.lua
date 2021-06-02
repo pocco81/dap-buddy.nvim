37677e2be651a9f1b0ff58f4209a3d9aca9b0cc0
kdav5758 <kdav5758@gmail.com>
2021-06-02 09:17:26 -0500

    testing configurations 2

--- lua/dap-install/init.lua
+++ lua/dap-install/init.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-
 local dap = require("dap")
 
 function M.setup(custom_opts)
@@ -13,32 +12,30 @@ function M.config(debugger, config)
     local dbg_list = require("dap-install.debuggers_list").debuggers
     local dbg = require(dbg_list[debugger][1])
 
-	print("Debugger = "..debugger.."; Config = "..tostring(config))
+    print("Debugger = " .. debugger .. "; Config = " .. tostring(config))
 
     if next(config) then
-		print("Got here!!")
+        print("Got here!!")
         for opt, _ in pairs(config) do
-			print("Opt = "..opt)
+            print("Opt = " .. opt)
             if (opt == "adapters") then
-					--                 for inner_opt, _ in pairs(opt["adapters"]) do
-					-- print("Inner opt (adapters) = "..inner_opt)
-					--                     if (dbg.config["config"][opt]["adapters"][inner_opt] ~= nil) then -- not nil
-					--                         config.options[opt]["adapters"][inner_opt] = config[opt][inner_opt]
-					--                     end
-					--                 end
+                --                 for inner_opt, _ in pairs(opt["adapters"]) do
+                -- print("Inner opt (adapters) = "..inner_opt)
+                --                     if (dbg.config["config"][opt]["adapters"][inner_opt] ~= nil) then -- not nil
+                --                         config.options[opt]["adapters"][inner_opt] = config[opt][inner_opt]
+                --                     end
+                --                 end
             elseif (opt == "configurations") then
-				-- should iterate through index one
-				print("Config -1 = "..tostring(config[opt]))
-				print("Config 0 = "..tostring(config[opt]["configurations"]))
-				print("Config 1 = "..tostring(config[opt]["configurations"][1]))
-					--                 for inner_opt, _ in pairs(opt["configurations"][1]) do
-					-- print("Inner opt (configurations) = "..inner_opt)
-					--                     if (dbg.config["config"][opt]["configurations"][1][inner_opt] ~= nil) then -- not nil
-					--                         config.options[opt]["configurations"][1][inner_opt] = config[opt][1][inner_opt]
-					--                     end
-					--                 end
-			else
-				print("Opt "..tostring(opt).." was not recognized!")
+                -- should iterate through index one
+                print("Config -1 = " .. tostring(config[opt]))
+                for inner_opt, _ in pairs(config[opt][1]) do
+                    print("Inner opt (configurations) = " .. inner_opt)
+                    -- if (dbg.config["config"][opt]["configurations"][1][inner_opt] ~= nil) then -- not nil
+                    --     config.options[opt]["configurations"][1][inner_opt] = config[opt][1][inner_opt]
+                    -- end
+                end
+            else
+                print("Opt " .. tostring(opt) .. " was not recognized!")
             end
         end
     end
