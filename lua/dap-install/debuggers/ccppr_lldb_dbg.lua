58eae7c709c560a8ab8eaca981c38e1abf3e292c
kdav5758 <kdav5758@gmail.com>
2021-06-02 16:24:32 -0500

    specified which ccppr debugger config 1

--- lua/dap-install/debuggers/ccppr_lldb_dbg.lua
+++ lua/dap-install/debuggers/ccppr_lldb_dbg.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local dbg_path = require("dap-install.debuggers_list").debuggers["ccppr_dbg"][2]
+local dbg_path = require("dap-install.debuggers_list").debuggers["ccppr_lldb_dbg"][2]
 
 M.dap_info = {
     name_adapter = "cpp"
