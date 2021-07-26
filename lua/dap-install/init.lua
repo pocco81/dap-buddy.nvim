295121a2b763a6ebf1d5b922a590e23f41b5bb6a
Pocco81 <pocco451@gmail.com>
2021-07-26 13:12:23 -0500

    added api

--- lua/dap-install/init.lua
+++ lua/dap-install/init.lua
@@ -3,6 +3,7 @@ local M = {}
 local dap = require("dap")
 local utils_tbl = require("dap-install.utils.tables.init")
 local utils_paths = require("dap-install.utils.paths.init")
+local di_api = require("dap-install.api.debuggers")
 
 function M.setup(custom_opts)
     require("dap-install.config").set_options(custom_opts)
@@ -10,7 +11,7 @@ end
 
 local function call_on_dap(debugger, dbg_list, user_config)
     local dbg = require(dbg_list[debugger][1])
-	local final_config = vim.tbl_deep_extend("force", dbg.config, user_config)
+    local final_config = vim.tbl_deep_extend("force", dbg.config, user_config)
 
     if (dbg.config["adapters"] ~= nil) then
         dap.adapters[dbg.dap_info["name_adapter"]] = final_config["adapters"]
@@ -23,11 +24,11 @@ end
 
 function M.config(debugger, config)
     config = config or {}
-    local dbg_list = require("dap-install.debuggers_list").debuggers
+    local dbg_list = di_api.get_debuggers()
 
     if (require("dap-install.config").options["verbosely_call_debuggers"] == true) then
-		print("DAPInstall: Passing the " .. debugger .. " to nvim-dap...")
-	end
+        print("DAPInstall: Passing the " .. debugger .. " to nvim-dap...")
+    end
 
     if utils_tbl.tbl_has_element(dbg_list, debugger, "index") then
         if (utils_paths.assert_dir(dbg_list[debugger][2]) == 1) then
