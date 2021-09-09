8f787085a1ec887bd502ed81f4bc2e93dd036cad
Pocco81 <pocco451@gmail.com>
2021-09-09 18:35:18 -0500

    dev: refactored core

--- lua/dap-install/main.lua
+++ lua/dap-install/main.lua
@@ -10,11 +10,11 @@ function M.main(option, command_option)
 	command_option = command_option or "NONE"
 
 	if (option == 0) then
-		require("dap-install.tools.install").install_debugger(command_option)
+		require("dap-install.core.install").install_debugger(command_option)
 	elseif (option == 1) then
-		require("dap-install.tools.uninstall").uninstall_debugger(command_option)
+		require("dap-install.core.uninstall").uninstall_debugger(command_option)
 	elseif (option == 2) then
-		require("dap-install.tools.list").list_debuggers()
+		require("dap-install.core.list").list_debuggers()
 	else
 		cmd("echo 'DAPInstall: Command was not recognized'")
 	end
