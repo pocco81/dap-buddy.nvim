d282d61bcc9bd685c4b571ac341a1951d358c3a0
Pocco81 <pocco451@gmail.com>
2021-09-09 20:28:12 -0500

    dev: format api

--- lua/dap-install/api/debuggers.lua
+++ lua/dap-install/api/debuggers.lua
@@ -3,20 +3,20 @@ local M = {}
 local utils_paths = require("dap-install.utils.paths.init")
 
 function M.get_debuggers()
-    return require("dap-install.debuggers_list").debuggers
+	return require("dap-install.debuggers_list").debuggers
 end
 
 function M.get_installed_debuggers()
-    local dbg_list = M.get_debuggers()
-    local installed_debuggers = {}
+	local dbg_list = M.get_debuggers()
+	local installed_debuggers = {}
 
-    for debugger, _ in pairs(dbg_list) do
-        if (utils_paths.assert_dir(dbg_list[debugger][2]) == 1) then
-            installed_debuggers[#installed_debuggers + 1] = debugger
-        end
-    end
+	for debugger, _ in pairs(dbg_list) do
+		if utils_paths.assert_dir(dbg_list[debugger][2]) == 1 then
+			installed_debuggers[#installed_debuggers + 1] = debugger
+		end
+	end
 
-    return installed_debuggers
+	return installed_debuggers
 end
 
 return M
