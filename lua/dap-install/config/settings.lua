0d86e9d3beba0f974a848a6f512a759e437717bd
Pocco81 <pocco451@gmail.com>
2021-09-09 20:29:39 -0500

    dev: format config

--- lua/dap-install/config/settings.lua
+++ lua/dap-install/config/settings.lua
@@ -1,17 +1,17 @@
 local settings = {}
 
 settings.options = {
-    installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
+	installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
 }
 
 function settings.set_options(opts)
-    opts = opts or {}
+	opts = opts or {}
 
-    for opt, _ in pairs(opts) do
-        if (settings.options[opt] ~= nil) then -- not nil
-            settings.options[opt] = opts[opt]
-        end
-    end
+	for opt, _ in pairs(opts) do
+		if settings.options[opt] ~= nil then
+			settings.options[opt] = opts[opt]
+		end
+	end
 end
 
 return settings
