99278ad428d1d62d289384d445bdbe97b85b9709
Pocco81 <pocco451@gmail.com>
2021-09-09 20:09:13 -0500

    fix: loader

--- lua/dap-install/core/loader.lua
+++ lua/dap-install/core/loader.lua
@@ -5,6 +5,7 @@ local utils_tbl = require("dap-install.utils.tables.init")
 local utils_paths = require("dap-install.utils.paths.init")
 local dbg_list = require("dap-install.core.debuggers_list").debuggers
 local cnf_sys = require("dap-install.config.sys").options
+local cnf_sett = require("dap-install.config.settings").options
 
 local function load(debugger, user_config)
 	local dbg = require(cnf_sys.dbgs_path .. debugger)
@@ -27,7 +28,7 @@ function M.config(debugger, config)
 	end
 
 	if utils_tbl.tbl_has_element(dbg_list, debugger, "index") then
-		if utils_paths.assert_dir(dbg_list[debugger][2]) == 1 then
+		if utils_paths.assert_dir(cnf_sett.installation_path .. debugger .. "/") == 1 then
 			load(debugger, config)
 		end
 	else
