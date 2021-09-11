2a23433216c3c968ea98d8a67cc266fb4eddfb9c
Pocco81 <pocco451@gmail.com>
2021-09-10 20:21:50 -0500

    fix #22: Where is the path in that list?

--- lua/dap-install/api/debuggers.lua
+++ lua/dap-install/api/debuggers.lua
@@ -9,10 +9,11 @@ end
 function M.get_installed_debuggers()
 	local dbg_list = M.get_debuggers()
 	local installed_debuggers = {}
+	local installation_path = require("dap-install.config.settings").options["installation_path"]
 
 	for debugger, _ in pairs(dbg_list) do
-		if utils_paths.assert_dir(dbg_list[debugger][2]) == 1 then
-			installed_debuggers[#installed_debuggers + 1] = debugger
+		if utils_paths.assert_dir(installation_path .. debugger .. "/") == 1 then
+			table.insert(installed_debuggers, debugger)
 		end
 	end
 
