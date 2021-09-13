036b11ac10b0053b99be8b6f509ba5ee5d439170
Pocco81 <pocco451@gmail.com>
2021-09-12 21:14:07 -0500

    dev: doc for handler

--- lua/dap-install/core/handlers.lua
+++ lua/dap-install/core/handlers.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+---handles exit statuses for terminals by returning a pointer to the handler
+---@param msg_error string
+---@param msg_success string
+---@return function
 function M.exit(msg_error, msg_success)
 	return function (_, code)
 		if code ~= 0 then
