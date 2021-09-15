fc96f909302fbe5fc8b29a6d717bcf070d0ce0af
Pocco81 <pocco451@gmail.com>
2021-09-14 22:19:54 -0500

    feat: funcs for asserting sys deps

--- lua/dap-install/utils/sys.lua
+++ lua/dap-install/utils/sys.lua
@@ -18,4 +18,31 @@ function M.get_shell()
 	return "/usr/bin/env bash"
 end
 
+-- original: https://stackoverflow.com/questions/132397/get-back-the-output-of-os-execute-in-lua
+-- source: https://gist.github.com/dukeofgaming/453cf950abd99c3dc8fc
+function M.return_exe(cmd, raw)
+	local handle = assert(io.popen(cmd, "r"))
+	local output = assert(handle:read("*a"))
+
+	handle:close()
+
+	if raw then
+		return output
+	end
+
+	output = string.gsub(string.gsub(string.gsub(output, "^%s+", ""), "%s+$", ""), "[\n\r]+", " ")
+
+	return output
+end
+
+function M.program_exists(program)
+	if
+		M.return_exe([[if command -v ]] .. program .. [[ &> /dev/null ; then echo "exists" ; fi
+]], false) == "exists"
+	then
+		return true
+	end
+	return false
+end
+
 return M
