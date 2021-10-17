97b3635dabe8cfbce8210d444a37a34e2b8eabf8
LebJe <51171427+LebJe@users.noreply.github.com>
2021-10-16 22:00:16 -0400

    Add configuration.

--- lua/dap-install/core/debuggers/codelldb.lua
+++ lua/dap-install/core/debuggers/codelldb.lua
@@ -71,6 +71,19 @@ M.config = {
 			end
 		end)
 	end,
+
+	configurations = {
+		{
+			name = "Launch file",
+			type = "codelldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = true,
+		},
+	},
 }
 
 M.installer = {
